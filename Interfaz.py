2cbdcfa598210eb59958108c10d98c122284b632
Augusto Guchea <aguchealezama@gmail.com>
2023-05-18 10:10:57 -0300

    Interfaz 2.4

--- Interfaz.py
+++ Interfaz.py
@@ -5,7 +5,7 @@ from idlelib.tooltip import Hovertip
 from main import Descarga_general
 
 class EntryWithPlaceholder(tk.Entry):
-    def __init__(self, master=None, placeholder="", color='#471F6F', *args, **kwargs):
+    def __init__(self, master=None, placeholder="", color='#DBEDFA', *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.placeholder = placeholder
         self.placeholder_color = color
@@ -37,33 +37,37 @@ root = tk.Tk()
 root.geometry("400x300")
 root.title("I2meta")
 root.resizable(False, False)
-root.configure(bg="#F7F3E3")
+root.configure(bg="#35355A")
 
 # Widgets
-entry_url = EntryWithPlaceholder(root, placeholder="Ingrese la URL", width=35, bg='#6FB8EC')
-entry_nombre = EntryWithPlaceholder(root, placeholder="Ingrese el nombre del archivo", width=35, bg='#6FB8EC')
-label_directorio = ttk.Label(root, text="Seleccione el directorio", width=25, background='#6FB8EC')
-##Cambiar el color del texto del label.
-directorio_button = tk.Button(root, text="Explorar", command=explorar_directorio, width=6, bg='#6FB8EC', fg='#F7F3E3')
-label_calidad = ttk.Label(root, text="Calidad:", background='#F7F3E3')
+entry_url = EntryWithPlaceholder(root, placeholder="Ingrese la URL", width=35, bg='#444474', highlightthickness=0, foreground='#DBEDFA')
+
+entry_nombre = EntryWithPlaceholder(root, placeholder="Ingrese el nombre del archivo", width=35, bg='#444474', highlightthickness=0, foreground='#DBEDFA')
+
+label_directorio = ttk.Label(root, text="Seleccione el directorio", width=25, background='#35355A', foreground="#DBEDFA")
+directorio_button = tk.Button(root, text="Explorar", command=explorar_directorio, width=6, bg='#444474', fg='#DBEDFA', highlightthickness=0)
+directorio_button.config(activebackground='#DBEDFA', activeforeground='#471F6F')
+
+label_calidad = ttk.Label(root, text="Calidad:",background='#35355A', foreground="#DBEDFA")
 calidades = ["Baja", "Media", "Alta"]
-##Cambiar el color del desplegable.
 var_calidad = tk.StringVar(root)
 var_calidad.set(calidades[0])
 option_calidad = tk.OptionMenu(root, var_calidad, *calidades)
-option_calidad.config(width=6, background='#6FB8EC', fg="#F7F3E3")
-label_formato = ttk.Label(root, text="Formato:", background='#F7F3E3')
+option_calidad.config(width=6, background='#DBEDFA', foreground='#444474',activebackground='#444474', activeforeground='#DBEDFA')
+option_calidad["menu"].config(bg='#DBEDFA', fg="#444474", activebackground='#444474', activeforeground='#DBEDFA')
+
+label_formato = ttk.Label(root, text="Formato:", background='#35355A', foreground="#F7F3E3")
 var_tipo = tk.StringVar()
 var_tipo.set("video")
-radio_video = ttk.Radiobutton(root, text="Video", variable=var_tipo, value="video")
-radio_audio = ttk.Radiobutton(root, text="Audio", variable=var_tipo, value="audio")
+radio_video = ttk.Radiobutton(root, text="Video", variable=var_tipo, value="video", style='RB.TRadiobutton' )
+radio_audio = ttk.Radiobutton(root, text="Audio", variable=var_tipo, value="audio", style='RB.TRadiobutton')
+
 ##Cambiar los fondos de los radiobutton.
-button_descargar = ttk.Button(root, command=lambda: Descarga_general(entry_url.get(), entry_nombre.get(), var_tipo.get()),text="Descargar", style='my.TButton', width=10)
-
+button_descargar = tk.Button(root, command=lambda: Descarga_general(entry_url.get(), entry_nombre.get(), var_tipo.get()),text="Descargar")
+button_descargar.config(width=10, background='#DBEDFA', foreground='#444474',activebackground='#444474', activeforeground='#DBEDFA')
 # Estilos
-style = ttk.Style()
-style.configure('my.TButton', font=('Segoe UI', 10), background='#6FB8EC', foreground='#F7F3E3', width=10)
-
+style_rb = ttk.Style()
+style_rb.configure('RB.TRadiobutton', foreground='#444474', background='#DBEDFA')
 # Posiciones
 entry_url.place(x=15, y=10)
 entry_nombre.place(x=15, y=50)
