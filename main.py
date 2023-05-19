87db5668faf09acd2a58d2e45cf34db1ea857eb9
Augusto Guchea <aguchealezama@gmail.com>
2023-05-19 08:23:04 -0300

    Interfaz 3.1

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ import traceback
 
 
 
-def descargar_video(url, titulo, calidad):
+def descargar_video(url, titulo, calidad, directorio):
     video = Video(url)
     filename = titulo+".mp4" if titulo else video.get_title()+".mp4"
-    video.download_video(calidad, Path.cwd(), filename)
+    video.download_video(calidad, directorio, filename)
     print(f"El {filename} de la url {url} se ha descargado correctamente.")
     return filename
 
 
-def descargar_audio(url , titulo):
+def descargar_audio(url , titulo, directorio):
     video = Video(url)
     filename = titulo+".mp4" if titulo else video.get_title()+".mp4"
-    video.download_audio(Path.cwd(), filename)
+    video.download_audio(directorio, filename)
     return filename
 
 def buscar_video(url: str, funcion):
@@ -29,16 +29,16 @@ def buscar_video(url: str, funcion):
     funcion(video.get_qualities_video())
 
 
-def Descarga_general(url, titulo, tipo, calidad):
+def Descarga_general(url, titulo, tipo, calidad, directorio):
     if tipo == "video":
         try:
-            filename = descargar_video(url, titulo, calidad)
+            filename = descargar_video(url, titulo, calidad, directorio)
             MessageBox.showinfo("Descarga completa", f"El archivo de video {filename} se ha descargado correctamente.")
         except Exception as e:
             MessageBox.showerror("Error", f"Se ha producido un error al descargar el archivo de video: {str(e)}")
     elif tipo == "audio":
         try:
-            filename = descargar_audio(url, titulo)
+            filename = descargar_audio(url, titulo, directorio)
             MessageBox.showinfo("Descarga completa", f"El archivo de audio {filename} se ha descargado correctamente.")
         except Exception as e:
             MessageBox.showerror("Error", f"Se ha producido un error al descargar el archivo de audio: {str(e)}")
